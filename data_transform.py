a4d0381f4d19faaf16e213490fb8b30694634f22
Felipe Milepe <dmilepe@gmail.com>
2016-06-11 11:38:02 -0300

    refatorando o código

--- data_transform.py
+++ data_transform.py
@@ -1,82 +1,95 @@
-import numpy as np
-from io import StringIO
-
-category_csv = open("Category.csv")
-categories = {}
-
-i =0
-for line in category_csv:
-    if i == 0:
-        for column in line.split(","):
-            categories[column.strip()] = []
-    else:
-        data = line.split(",")
-        categories[data[0].strip()] = {}
-        categories[data[0].strip()]["parentCategoryID"] = int(data[1].strip())
-    i+=1
-
-location_csv = open("Location.csv")
-locations = {}
-i =0
-for line in location_csv:
-    if i == 0:
-        for column in line.split(","):
-            locations[column.strip()] = []
-    else:
-        data = line.split(",")
-        locations[data[0].strip()] = {}
-        locations[data[0].strip()]["regionID"] = int(data[1].strip())
-    i+=1
-
-pairs_csv = open("ItemPairs_train.csv")
-pairs = {}
-i =0
-pairs["n_duplicates"] = 0
-for line in pairs_csv:
-    if i == 0:
-        for column in line.split(","):
-            pairs[column.strip()] = []
-    else:
-		data = line.split(",")
-		pairs["itemID_1"].append(int(data[0].strip()))
-		pairs["itemID_2"].append(int(data[1].strip()))
-		pairs["isDuplicate"].append(int(data[2].strip()))
-		if int(data[2].strip()) == 1 :
-			pairs["n_duplicates"] += 1
-		pairs["generationMethod"].append(int(data[3].strip()))
-    i+=1
-
-pairs["n_examples"] = len(pairs["isDuplicate"])
-
-print(pairs["n_duplicates"] * 1.0)/pairs["n_examples"],"% pares duplicados no dataset"
-
-items = {}
-
+#Importando modulos a serem usados no tratamento de dados
 import csv 
-with open("ItemInfo_train.csv","rb") as arq:
-	i = 0
-	items_data = csv.reader(arq,delimiter=',')
-	m_ids = []
-	for item in items_data:
-		if i > 0 :
-			items[item[0]] = {}
-			items[item[0]]["categoryID"] = item[1]
-			items[item[0]]["title"] = item[2]
-			items[item[0]]["description"] = item[3]
-			items[item[0]]["images_array"] = item[4].split(',')
-			items[item[0]]["price"] = item[6]
-			items[item[0]]["locationID"] = item[7]
-			items[item[0]]["metroID"] = item[8]
-			items[item[0]]["lat"] = item[9]
-			items[item[0]]["lon"] = item[10]
-			m_ids.append(item[8])
-			print (i)
-		if i > 200000:
-			break
-		i += 1
-	print "Numero de metroID's diferentes : ", len(set(m_ids))
+import numpy as np
 
-def transform_data_discrete_features(items,pairs,locations,categories):
+def parse_categories(filepath):
+	categories = {}
+	i =0
+	with open(filepath) as category_file:
+		category_csv = csv.reader(category_file,delimiter=",")
+		for line in category_csv:
+			if i == 0:
+				for column in line:
+				    categories[column.strip()] = []
+			else:
+				categories[line[0].strip()] = {}
+				categories[line[0].strip()]["parentCategoryID"] = int(line[1].strip())
+			i+=1
+	print "[LOG] ",i," categories parsed"
+
+def parse_locations(filepath):
+	locations = {}
+	i =0
+	with open(filepath) as location_file:
+		location_csv = csv.reader(location_file,delimiter=",")
+		for line in location_csv:
+			if i == 0:
+				for column in line:
+				    locations[column.strip()] = []
+			else:
+				locations[line[0].strip()] = {}
+				locations[line[0].strip()]["regionID"] = int(line[1].strip())
+			i+=1
+	print "[LOG] ",i," locations parsed"
+	
+	return locations
+
+def parse_pairs_file(filepath,train=True) :
+	"""
+		Recebe o nome do arquivo de pares de anuncios por parametro e o carrega para um dicionario com listas para cada campo. Cada lista esta ordenada de acordo com a linha do arquivo original a qual ela se refere.
+	"""
+	pairs = {}
+	i =0
+	pairs["n_duplicates"] = 0
+	with open(filepath,"rb") as pairs_arq:
+		pairs_csv = csv.reader(pairs_arq,delimiter=',')
+		for line in pairs_csv:
+			if i == 0:
+				for column in line:
+				    pairs[column.strip()] = []
+			else:
+				pairs["itemID_1"].append(int(line[0].strip()))
+				pairs["itemID_2"].append(int(line[1].strip()))
+				if train :
+					pairs["isDuplicate"].append(int(line[2].strip()))
+					if int(line[2].strip()) == 1 :
+						pairs["n_duplicates"] += 1
+				pairs["generationMethod"].append(int(line[3].strip()))
+			i+=1
+		if train :
+			pairs["n_examples"] = len(pairs["isDuplicate"])
+			
+			print "[DATA-INFO] ",(pairs["n_duplicates"] * 1.0)/pairs["n_examples"],"% pares duplicados no dataset"
+	print "[LOG] ", i, " pairs of ads parsed"
+	return pairs
+
+def parse_items(filepath):
+	items = {}
+	with open(filepath,"rb") as arq:
+		i = 0
+		items_data = csv.reader(arq,delimiter=',')
+		m_ids = []
+		for item in items_data:
+			if i > 0 :
+				items[item[0]] = {}
+				items[item[0]]["categoryID"] = item[1]
+				items[item[0]]["title"] = item[2]
+				items[item[0]]["description"] = item[3]
+				items[item[0]]["images_array"] = item[4].split(',')
+				items[item[0]]["price"] = item[6]
+				items[item[0]]["locationID"] = item[7]
+				items[item[0]]["metroID"] = item[8]
+				items[item[0]]["lat"] = item[9]
+				items[item[0]]["lon"] = item[10]
+				m_ids.append(item[8])
+			if i > 800000:
+				break
+			i += 1
+		print "[DATA-INFO] Numero de metroID's diferentes : ", len(set(m_ids))
+		print "[LOG] ",i, " items parsed"
+	return items
+
+def transform_data_discrete_features(items,pairs,locations,categories,file_output=False,outfile="merged_data.csv"):
 	""" 
 		Funcao que pre-processa os dados e gera um conjunto de dados somente com features discretas.
 	"""
@@ -94,7 +107,7 @@ def transform_data_discrete_features(items,pairs,locations,categories):
 		
 		#Calcula campo booleano que diz se os itens sao de mesma categoria
 		if item1["categoryID"] == item2["categoryID"] :
-			same_category = 1
+		sublie	same_category = 1
 		else :
 			same_category = 0
 		line.append(same_category)
---
+++ main.py
@@ -0,0 +1,5 @@
+from data_transform import *
+
+categories = parse_categories("Category.csv")
+locations = parse_locations("Location.csv")
+items = parse_items("ItemInfo_train.csv")
